043e4159ebe1e954bcaeec5443331853a5632aec
pedro <pedrosantosjovem2004@gmail.com>
2024-08-02 16:42:45 -0300

    init:inializado o nosso projeto saraiva com java e spring

---
+++ src/main/java/br/com/projeto/controller/DadosPessoaisController.java
@@ -0,0 +1,48 @@
+package br.com.projeto.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.domain.Dadospessoais;
+import br.com.projeto.repository.DadosPessoaisRepository;
+
+@RestController
+@RequestMapping("/dadospessoaias")
+public class DadosPessoaisController {
+
+	@Autowired
+	private DadosPessoaisRepository dpr;
+	
+	@PostMapping("/cadastrar")
+	public String cadastrar(@RequestBody Dadospessoais dp) {
+		dpr.save(dp);
+		return "Dados cadastrados";
+	}
+	@GetMapping("/listar")
+	public List<Dadospessoais> listar(){
+		return dpr.findAll();
+	}
+	
+	@GetMapping("/consultar/{id}")
+	public Optional<Dadospessoais> consultar(@PathVariable Integer id){
+		return dpr.findById(id);
+	}
+	
+	@GetMapping("/consular/{cpf}")
+	public Optional<Dadospessoais> consultarcpf(@PathVariable String cpf){
+		return dpr.findByCpf(cpf);
+	}
+	
+	@GetMapping("/consultar/{email}")
+	public Optional<Dadospessoais> consultaremail(@PathVariable String email){
+		return dpr.findByEmail(email);
+	}
+}
